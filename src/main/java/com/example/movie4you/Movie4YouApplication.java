9519d2cff937c175d9dd8c06a15625e9d35f958d
zackdarklord <zakaria.chelbi@esprit.tn>
2023-09-27 10:57:09 +0200

    structure

---
+++ src/main/java/com/example/movie4you/Movie4YouApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie4YouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie4YouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movie4you/Movie4YouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Movie4YouApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
